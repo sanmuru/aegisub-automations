1c6f1a2cbd5d44f43659012777d480bb29c84502
Sam Lu <549429518@qq.com>
2019-07-08 01:51:51 +0800

    Change indent from spaces to tabs

--- ChatroomEffect-generate.lua
+++ ChatroomEffect-generate.lua
@@ -17,36 +17,36 @@ log_debug = function(msg, ...) aegisub.log(4, msg, ...) end
 log_trace = function(msg, ...) aegisub.log(5, msg, ...) end
 
 local process_main = function(subtitles, selection)
-    local lines = {}
-    if selectedlines then
-        for i = 1, #selection do table.insert(lines, subtitles[selection[i]]) end
-    else
-        for i = 1, #subtitles do table.insert(lines, subtitles[i]) end
-    end
+	local lines = {}
+	if selectedlines then
+		for i = 1, #selection do table.insert(lines, subtitles[selection[i]]) end
+	else
+		for i = 1, #subtitles do table.insert(lines, subtitles[i]) end
+	end
 
-    do_layout(lines, actors, layouts)
+	do_layout(lines, actors, layouts)
 end
 
 local do_layout = function(lines, actors, layouts)
-    local xres, yres
-    meta = karaskel.collect_head(subtitles, generate_furigana)
-    if meta.res_x == nil or meta.res_y == nil then
-        xres, yres = aegisub.video_size()
-        if xres == nil or yres == nil then
-            aegisub.log()
+	local xres, yres
+	meta = karaskel.collect_head(subtitles, generate_furigana)
+	if meta.res_x == nil or meta.res_y == nil then
+		xres, yres = aegisub.video_size()
+		if xres == nil or yres == nil then
+			aegisub.log()
 
-        end
-    end
+		end
+	end
 
-    local line
-    if layouts[line.effect] ~= nil then
-        local layout = layouts[line.effect]
-        if layout.layer == nil then layout.layer = 0 end
-    end
+	local line
+	if layouts[line.effect] ~= nil then
+		local layout = layouts[line.effect]
+		if layout.layer == nil then layout.layer = 0 end
+	end
 end
 
 --[[ 测算布局
----- 参数：
+---- 参数：line, styles, layout, layer, rect, data
 	 line: 当前处理的行。
 	 styles: 已定义的所有样式。
 	 layout: 当前处理的布局。
@@ -59,9 +59,9 @@ end
 ---- ]]--
 local measure_layout = function(line, styles, layout, layer, rect, data)
 	-- 计算margin。
-    local margin
-    if layout.margin == nil then margin = { left = 0, right = 0, top = 0, bottom = 0 }
-    elseif tonumber(layout.margin) ~= nil then margin = { left = tonumber(layout.margin), right = tonumber(layout.margin), top = tonumber(layout.margin), bottom = tonumber(layout.margin) }
+	local margin
+	if layout.margin == nil then margin = { left = 0, right = 0, top = 0, bottom = 0 }
+	elseif tonumber(layout.margin) ~= nil then margin = { left = tonumber(layout.margin), right = tonumber(layout.margin), top = tonumber(layout.margin), bottom = tonumber(layout.margin) }
 	elseif type(layout.margin) == "string" and regexutil.find("\\s*,\\s*(\\d*\\.\\d*||\\d+)(\\s*(\\d*\\.\\d*||\\d+)){2,4}\\s*", layout.margin) then
 		regexresult = regexutil.find("(?:(^|,)\\s*)\\d*\\.\\d*||\\d+(?:\\s*($|,))", layout.margin)
 		if #regexresult.margin == 2 then
@@ -79,92 +79,92 @@ local measure_layout = function(line, styles, layout, layer, rect, data)
 				bottom = tonumber(regexresult[4])
 			}
 		else log_error("margin值的格式不正确。")
-        end
-    elseif type(layout.margin) == "table" then
-        if layout.margin.left or layout.margin.right or layout.margin.top or layout.margin.bottom then
-            margin = {
-                left = layout.margin.left or 0,
-                right = layout.margin.right or 0,
-                top = layout.margin.top or 0,
-                bottom = layout.margin.bottom or 0
-            }
-        elseif #layout.margin == 2 then
-            margin = {
+		end
+	elseif type(layout.margin) == "table" then
+		if layout.margin.left or layout.margin.right or layout.margin.top or layout.margin.bottom then
+			margin = {
+				left = layout.margin.left or 0,
+				right = layout.margin.right or 0,
+				top = layout.margin.top or 0,
+				bottom = layout.margin.bottom or 0
+			}
+		elseif #layout.margin == 2 then
+			margin = {
 				left = layout.margin[1],
 				right = layout.margin[1],
 				top = layout.margin[2],
 				bottom = layout.margin[2]
 			}
-        elseif #layout.margin == 4 then
-            margin = {
-                left = layout.margin[1],
-                right = layout.margin[2],
-                top = layout.margin[3],
-                bottom = layout.margin[4]
-            }
-        else log_error("margin值的格式不正确。")
-        end
-    else samlu.log.error("margin值的格式不正确。")
-    end
-    
+		elseif #layout.margin == 4 then
+			margin = {
+				left = layout.margin[1],
+				right = layout.margin[2],
+				top = layout.margin[3],
+				bottom = layout.margin[4]
+			}
+		else log_error("margin值的格式不正确。")
+		end
+	else samlu.log.error("margin值的格式不正确。")
+	end
+	
 	-- 计算宽度和高度的最小值和最大值。
-    local minwidth, maxwidth, minheight, maxheight
-    if layout.minwidth == nil then minwidth = 0
-    elseif tonumber(layout.minwidth) ~= nil then
-        minwidth = tonumber(layout.minwidth)
-        if minwidth < 0 then log_error("minwidth值不应小于零。") end
-    else samlu.log.error("minwidth值的格式不正确。")
-    end
-    if layout.maxwidth == nil then ;
-    elseif tonumber(layout.maxwidth) ~= nil then
-        maxwidth = tonumber(layout.maxwidth)
-        if maxwidth < minwidth then log_error("maxwidth值不应小于minwidth。") end
-    else samlu.log.error("minwidth值的格式不正确。")
-    end
-    if layout.minheight == nil then minheight = 0
-    elseif tonumber(layout.minheight) ~= nil then
-        minheight = tonumber(layout.minheight)
-        if minheight < 0 then log_error("minheight值不应小于零。") end
-    else samlu.log.error("minheight值的格式不正确。")
-    end
-    if layout.maxheight == nil then ;
-    elseif tonumber(layout.maxheight) ~= nil then
-        maxheight = tonumber(layout.maxheight)
-        if maxheight < minheight then log_error("maxheight值不应小于minheight。") end
-    else samlu.log.error("minheight值的格式不正确。")
-    end
+	local minwidth, maxwidth, minheight, maxheight
+	if layout.minwidth == nil then minwidth = 0
+	elseif tonumber(layout.minwidth) ~= nil then
+		minwidth = tonumber(layout.minwidth)
+		if minwidth < 0 then log_error("minwidth值不应小于零。") end
+	else samlu.log.error("minwidth值的格式不正确。")
+	end
+	if layout.maxwidth == nil then ;
+	elseif tonumber(layout.maxwidth) ~= nil then
+		maxwidth = tonumber(layout.maxwidth)
+		if maxwidth < minwidth then log_error("maxwidth值不应小于minwidth。") end
+	else samlu.log.error("minwidth值的格式不正确。")
+	end
+	if layout.minheight == nil then minheight = 0
+	elseif tonumber(layout.minheight) ~= nil then
+		minheight = tonumber(layout.minheight)
+		if minheight < 0 then log_error("minheight值不应小于零。") end
+	else samlu.log.error("minheight值的格式不正确。")
+	end
+	if layout.maxheight == nil then ;
+	elseif tonumber(layout.maxheight) ~= nil then
+		maxheight = tonumber(layout.maxheight)
+		if maxheight < minheight then log_error("maxheight值不应小于minheight。") end
+	else samlu.log.error("minheight值的格式不正确。")
+	end
 	
 	-- 计算宽度和高度。
-    local width, height
-    if tonumber(layout.width) == nil then -- 非数字类型值
-        if unicode.to_lower_case(layout.width) == "fill" then width = math.max(0, rect.width - margin.left - margin.right) -- 填充
-        elseif unicode.to_lower_case(layout.width) ~= "auto" then log_error("width值应为数值、fill或auto。")
-        --else -- 自动
-        end
-    else -- 数字类型值
-        if type(layout.width) == "number" then width = layout.width
-        else width = tonumber(layout.width)
-        end
-        if width < 0 then log_error("width值不应小于零。") end
-    end
-    if width < minwidth then width = minwidth
-    elseif maxwidth ~= nil and width > maxwidth then width = maxwidth
-    end
+	local width, height
+	if tonumber(layout.width) == nil then -- 非数字类型值
+		if unicode.to_lower_case(layout.width) == "fill" then width = math.max(0, rect.width - margin.left - margin.right) -- 填充
+		elseif unicode.to_lower_case(layout.width) ~= "auto" then log_error("width值应为数值、fill或auto。")
+		--else -- 自动
+		end
+	else -- 数字类型值
+		if type(layout.width) == "number" then width = layout.width
+		else width = tonumber(layout.width)
+		end
+		if width < 0 then log_error("width值不应小于零。") end
+	end
+	if width < minwidth then width = minwidth
+	elseif maxwidth ~= nil and width > maxwidth then width = maxwidth
+	end
 	if tonumber(layout.height) == nil then -- 非数字类型值
-        if unicode.to_lower_case(layout.height) == "fill" then height = math.max(0, rect.height - margin.top - margint.bottom) -- 填充
-        elseif unicode.to_lower_case(layout.height) ~= "auto" then log_error("height值应为数值、fill或auto。")
-        --else -- 自动
-        end
-    else -- 数字类型值
-        if type(layout.height) == "number" then height = layout.height
-        else height = tonumber(layout.height)
-        end
-        if height < 0 then log_error("height值不应小于零。") end
-    end
-    if height < minheight then height = minheight
-    elseif maxheight ~= nil and height > maxheight then height = maxheight
-    end
-    -- 若宽度或高度为auto，具体数值需要根据不同布局分别计算得出，故此时为nil。
+		if unicode.to_lower_case(layout.height) == "fill" then height = math.max(0, rect.height - margin.top - margint.bottom) -- 填充
+		elseif unicode.to_lower_case(layout.height) ~= "auto" then log_error("height值应为数值、fill或auto。")
+		--else -- 自动
+		end
+	else -- 数字类型值
+		if type(layout.height) == "number" then height = layout.height
+		else height = tonumber(layout.height)
+		end
+		if height < 0 then log_error("height值不应小于零。") end
+	end
+	if height < minheight then height = minheight
+	elseif maxheight ~= nil and height > maxheight then height = maxheight
+	end
+	-- 若宽度或高度为auto，具体数值需要根据不同布局分别计算得出，故此时为nil。
 	
 	-- 计算横向和纵向对齐。
 	local horizontalalignment, verticalalignment
@@ -213,8 +213,8 @@ local measure_layout = function(line, styles, layout, layer, rect, data)
 		avaliablerect.y = rect.y + rect.height - (avaliablerect.height + margin.top + margin.bottom)
 	end
 	
-    if layout.layouttype == "text" then -- 文本布局
-        local style = style_parse(styles, layout.style) -- 获取文本使用的样式。
+	if layout.layouttype == "text" then -- 文本布局
+		local style = style_parse(styles, layout.style) -- 获取文本使用的样式。
 		local wrappedtextminsize, wrappedtext = text_layout(style, avaliablerect, data.text) -- 进行文本布局。
 		
 		-- 计算文本的横向和纵向对齐。
@@ -250,7 +250,7 @@ local measure_layout = function(line, styles, layout, layer, rect, data)
 			width = wrappedtextminsize.width + margin.left + margin.right,
 			height = wrappedtextminsize.height + margin.top + margin.bottom
 		}
-    elseif layout.layouttype == "flow" then -- 流式布局
+	elseif layout.layouttype == "flow" then -- 流式布局
 		-- 计算横向和纵向间距。
 		local horizontalspacing = tonumber(layout.horizontalspacing) or 0
 		local verticalspacing = tonumber(layout.verticalspacing) or 0
@@ -321,7 +321,7 @@ local measure_layout = function(line, styles, layout, layer, rect, data)
 			getflowlinecontentheight = function(contentminsize) return contentminsize.width end
 		end
 		for index, contentlayout in ipairs(layout) do
-            local contentlayer = contentlayout.layer or layer + 1
+			local contentlayer = contentlayout.layer or layer + 1
 			contentresult, contentminsize = measure_layout(line, styles, contentlayout, contentlayer, avaliablerect, data[index])
 			if #flowline == 0 or flowlinelength + flowlinelengthspacing + getflowlinecontentlength(contentminsize) <= maxflowlinelength then
 				table.insert(flowline, {
@@ -502,7 +502,7 @@ local style_parse = function(styles, style)
 end
 
 local text_layout = function(style, size, data)
-    regexresult = regexutil.match("\\s+(?=\\S|$)|[\\dA-Za-z]+(?=[^\\dA-Za-z]|$)|\\b.+?\\b|\\S+(?=\\s|$)", data)
+	regexresult = regexutil.match("\\s+(?=\\S|$)|[\\dA-Za-z]+(?=[^\\dA-Za-z]|$)|\\b.+?\\b|\\S+(?=\\s|$)", data)
 	
 	local linebuffer = {}
 	local spanbuffer = {}
